f687f0f6d7e77cf153a7d7131f0177ce761cffa0
Claus <claus@kromedio.de>
2021-03-08 21:26:20 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+var part1 = "We're in here for ";
+var part2 = "JavaScript";
+document.write(part1 + part2);
+var name = "Robin";
+var age = 42;
+console.log(name);
+console.log("Oh, ich schreibe in die Console!")
